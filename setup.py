4bfbfa6859ce896ec6caff968a972c8b285f709b
Patrick Dizon <patrick.dizon@bbva.com>
2019-12-31 01:18:40 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='pautils',
-   version='0.0.3',
+   version='0.0.4',
    description='Proxy Analytics Utils',
    author='Proxy Analytics Engineering',
    packages=setuptools.find_packages(),
