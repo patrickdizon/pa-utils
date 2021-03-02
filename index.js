e616d9be70fd84e435ffb950a645f04de95ebb76
patrickdizon <prdizon@gmail.com>
2021-03-02 15:34:54 -0800

    move

---
+++ index.js
@@ -0,0 +1,13 @@
+const logger = require('./utils/logger')
+const lambda = require('./utils/lambda')
+const env = require('./utils/env')
+const string = require('./utils/string')
+const comparison = require('./utils/comparison')
+
+module.exports = {
+  logger,
+  lambda,
+  env,
+  string,
+  comparison,
+}
